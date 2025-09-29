b2ffc3cb1af1e29799d078c09b2e37c58d45335f
Eddy <limae481@gmail.com>
2025-09-29 14:29:11 +0200

    Mount test

--- custom/plugins/MyPluginEddy/src/ScheduledTask/ShowTimeTask.php
+++ custom/plugins/MyPluginEddy/src/ScheduledTask/ShowTimeTask.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace  MyPluginEddy\ScheduledTask;
+namespace MyPluginEddy\ScheduledTask;
 
 use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTask;
 
--- custom/plugins/MyPluginEddy/src/ScheduledTask/ShowTimeTaskHandler.php
+++ custom/plugins/MyPluginEddy/src/ScheduledTask/ShowTimeTaskHandler.php
@@ -5,14 +5,17 @@ namespace MyPluginEddy\ScheduledTask;
 
 use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskHandler;
 use MyPluginEddy\Service\FilesystemService;
+use Psr\Log\LoggerInterface;
 
 class ShowTimeTaskHandler extends ScheduledTaskHandler
 {
     private FilesystemService $filesystem;
+    private LoggerInterface $logger;
 
-    public function __construct(FilesystemService $filesystem)
+    public function __construct(FilesystemService $filesystem, LoggerInterface $logger)
     {
         $this->filesystem = $filesystem;
+        $this->logger = $logger;
     }
 
     public static function getHandledMessages(): iterable
@@ -22,9 +25,13 @@ class ShowTimeTaskHandler extends ScheduledTaskHandler
 
     public function run(): void
     {
+        // Schreiben in eigene Datei
         $this->filesystem->writeFile(
             'logs/showtime.log',
             "[" . date('Y-m-d H:i:s') . "] Task wurde ausgeführt\n"
         );
+
+        // Schreiben ins Shopware-Log (var/log/prod.log)
+        $this->logger->info('ShowTimeTaskHandler wurde ausgeführt!');
     }
 }
